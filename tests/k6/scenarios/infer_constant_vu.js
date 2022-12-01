a00c72f02936ee33641ca9e477e0a0d9210fe7a8
Sherif Akoush <sa@seldon.io>
2022-12-01 17:28:54 +0000

    revert back change to threshold

--- tests/k6/scenarios/infer_constant_vu.js
+++ tests/k6/scenarios/infer_constant_vu.js
@@ -30,7 +30,7 @@ export default function (config) {
     const modelNameWithVersion = modelName + getVersionSuffix(config)  // first version
 
     const rand = Math.random()
-    if (rand > 1) {
+    if (rand > 0.5) {
         doInfer(modelName, modelNameWithVersion, config, true)
     } else {
         doInfer(modelName, modelNameWithVersion, config, false)
